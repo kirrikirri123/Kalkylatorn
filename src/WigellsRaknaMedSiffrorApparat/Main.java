d0b116f06d9e0d96922144c65cd51ef20915e2ba
Kristina Ahlenius <kristinaahlenius@gmail.com>
2025-09-25 15:18:33 +0200

    first commit

---
+++ src/WigellsRaknaMedSiffrorApparat/Main.java
@@ -0,0 +1,8 @@
+package WigellsRaknaMedSiffrorApparat;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Välkomna till Wigells räkna med siffror apparat");
+
+    }
+}
