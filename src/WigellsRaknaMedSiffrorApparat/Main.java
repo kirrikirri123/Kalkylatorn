ab35369f408fac15a9165f87fab4048604ff7fe8
Kristina Ahlenius <kristinaahlenius@gmail.com>
2025-09-25 21:35:08 +0200

    Skapat metod Multiplikation, uppdaterat meny

--- src/WigellsRaknaMedSiffrorApparat/Main.java
+++ src/WigellsRaknaMedSiffrorApparat/Main.java
@@ -28,16 +28,20 @@ public class Main {
             System.out.println();
             break;
             case 3 : System.out.println("Vilka två tal vill du få produkten utav? Avskilj faktorerna med ett mellanslag, avsluta med enter.");
+                double userMultiFirst = scanner.nextDouble(); double userMultiSecond = scanner.nextDouble();
+                kalkylMetod.mulit(userMultiFirst,userMultiSecond);
             System.out.println("________________________________________________");
             System.out.println();
             break;
-            case 4 : System.out.println("Vilka två tal vill du få kvoten utav? Avskilj med mellanslag, avsluta me enter.") ; // /
+            case 4 : System.out.println("Vilka två tal vill du få kvoten utav? Avskilj med mellanslag, avsluta med enter.") ; // /
             System.out.println("________________________________________________");
             System.out.println();
             break;
             case 5 : System.out.println(" Avslutar programmet \n Tack för att du använde Wigells räkna-ihop-siffror-apparat.");
             userChoice = false;
-            default : System.out.println(" Åh nej, något blev fel. Prova att ange vald siffran till vänster om vald räknemetod i menyn ");
+                //default : System.out.println(" Åh nej, något blev fel. Prova att ange vald siffran till vänster om vald räknemetod i menyn ");
+
+
         }
 
       // }
