1d834efe65a00448ab36bf8fd0bd06c14590ebc7
Kristina Ahlenius <kristinaahlenius@gmail.com>
2025-09-29 13:04:03 +0200

    fix modulus metod,  ny loop inom case, uppdaterat text.

--- src/WigellsRaknaMedSiffrorApparat/KalkylatorMethods.java
+++ src/WigellsRaknaMedSiffrorApparat/KalkylatorMethods.java
@@ -1,40 +1,49 @@
 package WigellsRaknaMedSiffrorApparat;
 
-import java.util.Scanner;
-
 public class KalkylatorMethods {
 
-    public void MainMenu(String x ){
+    public void MainMenu(String x) {
 
         System.out.println("Välj en av räknemetoderna.\nSkriv in funktionens nummer och bekräfta med enter.\n");
-        System.out.println("[1] Addition");
-        System.out.println("[2] Subtraktion");
-        System.out.println("[3] Multiplikation");
-        System.out.println("[4] Division");
-        System.out.println("[5] Modulus");
+        System.out.println("[1] + Addition + ");
+        System.out.println("[2] - Subtraktion -");
+        System.out.println("[3] * Multiplikation *");
+        System.out.println("[4] / Division / ");
+        System.out.println("[5] % Modulus % ");
         System.out.println("[6] Avsluta programmet!");
     }
 
 
     // Addition
-    public void add(double x, double y){
-        System.out.println("Summan av talen blir = " +(x+y));
+    public void add(double x, double y) {
+        System.out.println("Summan av talen blir = " + (x + y));
     }
+
     // Subtraktion
-    public void sub(double x,double y) {
-        System.out.println("Differensen av talen blir = " + (x-y));
+    public void sub(double x, double y) {
+        System.out.println("Differensen av talen blir = " + (x - y));
     }
+
     // Multiplikation
-    public void mulit(double x, double y){
-        System.out.println("Produkten av talen blir = "+ (x*y));
-    }
-    //Division
+    public void mulit(double x, double y) {
+        System.out.println("Produkten av talen blir = " + (x * y));
+    }  //Division
+
     public void div(double x, double y) {
-        System.out.println("Kvoten av talen blir = "+(x/y));
-    }
+        System.out.println("Kvoten av talen blir = " + (x / y));
+    } // hur göra när användare använder 0? Infinity
+
     // Modulus
-    public void modu(double x,double y) {System.out.println("Resten ur ditt dividerade tall blir = "+(x%y));}
+    public void modu(double x, double y) {
+        if (x <= y) {
+            System.out.println("Täljaren i ditt tal måste vara större än nämnaren, försök igen ! ");
+        } else if (x == 0 || y == 0) {
+            System.out.println("Tyvärr, modulus går ej beräkna med talet noll, försök igen");
+        } else {
+            System.out.println("Resten ur ditt dividerade tall blir = " + (x % y));
+        }
+    }
+
+
+
 }
-
-
-
--- src/WigellsRaknaMedSiffrorApparat/Main.java
+++ src/WigellsRaknaMedSiffrorApparat/Main.java
@@ -9,44 +9,75 @@ public class Main {
         String start = "\nVälkommen till Wigells räkna-med-siffor-apparat!\n";
 
        boolean userChoice = true;
+
+
        System.out.println(start);
        while (userChoice) {
-        kalkylMetod.MainMenu(start);
+           boolean addAgain = true;
+           boolean subAgain = true;
+           boolean multiAgain= true;
+           boolean divAgain = true;
+           boolean moduAgain = true;
+                  kalkylMetod.MainMenu(start);
         System.out.print("\t\t\t");
-        int menuScan = scanner.nextInt();System.out.println("------> ------->");
+        String userMenuIn =scanner.next();
+        int menuScan = userMenuIn.charAt(0);
+
+
+        System.out.println("------> ------->");
 
         switch (menuScan) {
-            case 1 : System.out.println("Vilka två tal vill du få summan utav? Avskilj termerna med mellanslag, avsluta med enter.");
+            case '1' :
+            case '+':
+                while (addAgain){System.out.println("Vilka två tal vill du få summan utav? Avskilj termerna med mellanslag eller enter.");
                 double userAddFirst = scanner.nextDouble(); double userAddSec = scanner.nextDouble();
                 kalkylMetod.add(userAddFirst, userAddSec);
-                System.out.println("________________________________________________");
-                System.out.println();
+            System.out.println("________________________________________________");
+            System.out.println();
+            System.out.println("Addition igen? Skriv in JA");
+            String userAdd = scanner.next();if (!userAdd.equalsIgnoreCase("JA")){addAgain= false;}}
                 break;
-            case 2  : System.out.println("Vilka två tal vill du få differensen utav? Avskilj termerna med mellanslag, avsluta med enter.");
+            case '2':
+            case '-':
+                while (subAgain){ System.out.println("Vilka två tal vill du få differensen utav? Avskilj termerna med mellanslag eller enter.");
                 double userSubFirst = scanner.nextDouble(); double userSubSecond = scanner.nextDouble();
                 kalkylMetod.sub(userSubFirst,userSubSecond);
             System.out.println("________________________________________________");
             System.out.println();
+                System.out.println("Subtraktion igen? Skriv in JA");
+                String userSub = scanner.next();if (!userSub.equalsIgnoreCase("JA")){subAgain= false;}}
             break;
-            case 3 : System.out.println("Vilka två tal vill du få produkten utav? Avskilj faktorerna med ett mellanslag, avsluta med enter.");
+            case '3':
+            case '*':
+                while (multiAgain){System.out.println("Vilka två tal vill du få produkten utav? Avskilj faktorerna med mellanslag eller enter.");
                 double userMultiFirst = scanner.nextDouble(); double userMultiSecond = scanner.nextDouble();
                 kalkylMetod.mulit(userMultiFirst,userMultiSecond);
             System.out.println("________________________________________________");
             System.out.println();
+                System.out.println("Multiplikation igen? Skriv in JA");
+                String userMulti = scanner.next();if (!userMulti.equalsIgnoreCase("JA")){multiAgain= false;}}
             break;
-            case 4 : System.out.println("Vilka två tal vill du få kvoten utav? Avskilj med mellanslag, avsluta med enter.");
-            double userDivFirst = scanner.nextDouble(); double userDivSecond = scanner.nextDouble();;
-            kalkylMetod.div(userDivFirst, userDivSecond);
+            case '4':
+            case '/':
+                while (divAgain){ System.out.println("Vilka två tal vill du få kvoten utav? Avskilj med mellanslag eller enter.");
+                double userDivFirst = scanner.nextDouble(); double userDivSecond = scanner.nextDouble();;
+                kalkylMetod.div(userDivFirst, userDivSecond);
             System.out.println("________________________________________________");
             System.out.println();
+                System.out.println("Division igen? Skriv in JA");
+                String userDiv = scanner.next();if (!userDiv.equalsIgnoreCase("JA")){divAgain= false;}}
             break;
-            case 5 : System.out.println("Modulus räknar ut resten efter du dividerat två tal. Vilka två tal vill du testa ? Avskilj med mellanslag, avsluta med enter.");
+            case '5':
+            case '%':
+                while (moduAgain){System.out.println("Modulus räknar ut resten efter du dividerat två tal. Vilka två tal vill du testa ? Avskilj med mellanslag eller enter.");
                 double userModFirst = scanner.nextDouble(); double userModSecond = scanner.nextDouble();;
                 kalkylMetod.modu(userModFirst, userModSecond);
             System.out.println("________________________________________________");
             System.out.println();
+                System.out.println("Modulus igen? Skriv in JA");
+                String userModu = scanner.next();if (!userModu.equalsIgnoreCase("JA")){moduAgain= false;}}
             break;
-            case 6 : System.out.println(" Avslutar programmet ! \n Tack för att du använde Wigells räkna-ihop-siffror-apparat.");
+            case '6' : System.out.println(" Avslutar programmet ! \n Tack för att du använde Wigells räkna-med-siffror-apparat.");
             userChoice = false;
             break;
                 default : System.out.println(" Åh nej, något blev fel! Prova att ange siffran till vänster om vald räknemetod i menyn. ");
